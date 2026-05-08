acfcf98e096f250472ec16484a9c9b2feb02615c
OVERLORDxx <ks14635142@gmail.com>
2026-05-08 22:19:12 +0530

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: 'https://library-backend-5ipk.onrender.com/api',
 });
 
 API.interceptors.request.use((config) => {
